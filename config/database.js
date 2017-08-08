73409e92763d7344fa64cb02e56a3977555eaa4a
inkedingroup <30823083+inkedingroup@users.noreply.github.com>
2017-08-08 00:21:17 -0700

    First Commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  database: "mongodb://inkedingroup:inkedin123@ds127783.mlab.com:27783/inkeddb",
+  secret: "yoursecret"
+};
+//mongodb://inkedingroup:inkedin123@ds127783.mlab.com:27783/inkeddb
+//mongodb://admin:rebecca@ds113670.mlab.com:13670/inkedplay
